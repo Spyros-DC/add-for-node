507c5157ea7de3db357053de6939f02da65fb80f
Spyros-DC <sitedesigns17@gmail.com>
2015-07-10 13:34:21 +0300

    third commit

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ var app = express();
 var searchAddOn = require("./node_addon/build/Release/booksfinder.node");
 app.use(express.static(__dirname + '/public'));        
 var bodyParser = require('body-parser');
-// create application/json parser 
-var jsonParser = bodyParser.json()
  
 // create application/x-www-form-urlencoded parser 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
+var port = process.env.OPENSHIFT_NODEJS_PORT;
 app.get('/', function(req, res){
     res.render();
 });
@@ -17,6 +15,6 @@ app.post('/', urlencodedParser, function(req, res){
     res.json(searchAddOn.searchtitle(req.body.query));
 });
 
-app.listen(3000, function(){
-    console.log('listening on port 3000');
+app.listen(port, function(){
+    console.log('listening on port: ' + port);
 });
